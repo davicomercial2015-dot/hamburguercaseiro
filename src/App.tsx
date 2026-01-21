5a4fcc23d86c8deb78021b8565c1e3668293d6f9
davicomercial2015-dot <davicomercial2015@gmail.com>
2026-01-21 10:02:08 -0300

    Added tsconfig.node.json

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>Welcome</h1>
+        <p>Your app is running!</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
